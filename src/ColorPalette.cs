832f6e128eab0b88328463888bf16d15e14656e8
Radenz <radenrifqirahman@gmail.com>
2022-03-13 11:45:03 +0700

    refactor: Add abstract modifier to ColorPalette

--- src/ColorPalette.cs
+++ src/ColorPalette.cs
@@ -3,7 +3,7 @@ using Microsoft.Msagl.Drawing;
 
 namespace FolderCrawler
 {
-    public class ColorPalette
+    public abstract class ColorPalette
     {
         public static readonly Color RED = new Color(Byte.MaxValue, 0, 0);
         public static readonly Color BLUE = new Color(0, 0xb0, Byte.MaxValue);
